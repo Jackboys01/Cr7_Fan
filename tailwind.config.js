0bb2da66c916f57e1cd5499307312606e297d4a7
Jackboys01 <101064316+Jackboys01@users.noreply.github.com>
2023-10-11 20:34:03 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      maxWidth: {
+        'screen-md': '35rem', // You can adjust the value as needed
+      },
+      height: {
+        '40': '40px', // Custom height class for 40px
+      },
+    },
+    backgroundImage: {
+      'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      'gradient-conic':
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    },
+  },
+  plugins: [],
+}
